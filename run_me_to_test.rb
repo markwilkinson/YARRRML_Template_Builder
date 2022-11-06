527d5ec7d3e88d7d01739b844a7d41d4f1985662
Mark Wilkinson <markw@illuminae.com>
2022-11-06 13:57:31 +0100

    Update run_me_to_test.rb

--- run_me_to_test.rb
+++ run_me_to_test.rb
@@ -4,7 +4,7 @@ require "yarrrml-template-builder"
 
 $stderr.puts "this script will do an rdf transformation of the height.csv file, in the ./data folder using the yarrrml file in the ./config folder"
 $stderr.puts "you must already have the docker-compose up before running this script.  If you see failures, that is likely why :-)"
-datatype_tag = "height"  # the "tag" of your data
+datatype_tag = "height"  # the "tag" of your data (the filename without the '.csv' e.g. "grazing" for "grazing.csv")
 data_path_client = "./data"
 data_path_server = "/data"  # assumes you are running my docker image
 config_path_client = "./config"
@@ -13,7 +13,7 @@ rdfizer_base_url = "http://localhost:4000"  # again, my docker
 yarrrml_transform_base_url = "http://localhost:3000"  # again, my docker
 
 y = YARRRML_Transform.new(
-	datatype_tag: datatype_tag, 
+	datatype_tag: datatype_tag,
 	data_path_client: data_path_client,
 	data_path_server: data_path_server,
 	config_path_client: config_path_client,
